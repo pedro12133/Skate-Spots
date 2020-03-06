a67e0e04c8ac448645e36dbddaff8a5e0894f971
pedro12133 <pedrobeltran12133@gmail.com>
2020-03-05 21:00:41 -0800

    Custom markers save to Firebase.

--- lib/search_map.dart
+++ lib/search_map.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'data/error.dart';
 import 'data/place_response.dart';
 import 'data/result.dart';
@@ -28,7 +30,6 @@ class _SearchMap extends State<SearchMap> {
   static double zoom = 12;
   static const String baseUrl =
       "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
-
   final databaseRef = Firestore.instance;
   List<Marker> markers = [];
   Error error;
@@ -37,6 +38,7 @@ class _SearchMap extends State<SearchMap> {
   String keyword;
   int locationCount = 0;
   GoogleMapController _mapController;
+  bool editingMarker = false;
 
   setMarkers() {
     return markers;
@@ -45,15 +47,6 @@ class _SearchMap extends State<SearchMap> {
   @override
   void initState(){
     super.initState();
-    markers.add(Marker(
-      markerId: MarkerId(''),
-      draggable: true,
-
-      onTap: () {
-        print('Marker Tapped');
-      },
-      position: LatLng(lat,lon),
-    ));
   }
 
 
@@ -79,11 +72,15 @@ class _SearchMap extends State<SearchMap> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _panToCurrentLocation(_mapController);
-          print("prev: "+lat.toString()+lon.toString());
-          setCameraToCurrentLocation(lat,lon);
-          print("current: "+lat.toString()+lon.toString());
-          searchNearby(lat, lon);
+          if(!editingMarker) {
+            _panToCurrentLocation(_mapController);
+            print("prev: "+lat.toString()+lon.toString());
+            setCameraToCurrentLocation(lat,lon);
+            print("current: "+lat.toString()+lon.toString());
+            searchNearby(lat, lon);
+          }
+          else
+            popupMessage("Attributes for current marker aren\'t set.");
         },
         backgroundColor: Colors.grey,
         child: Image.asset("assets/images/skate.png"),
@@ -99,13 +96,24 @@ class _SearchMap extends State<SearchMap> {
           height: 56.0,
           child: Row(children: <Widget>[
             IconButton(
-              onPressed: _getSavedSpots,
+              onPressed: () {
+                if(!editingMarker)
+                  showMenu();
+                else
+                  popupMessage("Attributes for current marker aren\'t set.");
+              },
               icon: Icon(Icons.list),
               color: Colors.white,
             ),
             Spacer(),
             IconButton(
-              onPressed: _saveSpot,
+              onPressed: () {
+                if(!editingMarker)
+                  _addMarker();
+                else
+                  popupMessage("Attributes for current marker aren\'t set.");
+
+              },
               icon: Icon(Icons.add_location),
               color: Colors.white,
             )
@@ -121,31 +129,228 @@ class _SearchMap extends State<SearchMap> {
         snapshot.documents.forEach((f) => print('${f.data}'));
     });
   }
-  void _saveSpot() async {
+  void _saveSpot(String category, String label, GeoPoint coordinates) async {
     print('saving...');
-    await databaseRef.collection('markers')
-        .add({
-          'category' : 'rail',
-          'coordinates': new GeoPoint(20, 12),
-          'place' : 'Hollywood 8'
-        });
-    print('Saved!.');
+    try {
+      await databaseRef.collection('markers')
+          .add({
+        'category' : category,
+        'coordinates': coordinates,
+        'place' : label
+      });
+      print('Saved!.');
+      Scaffold.of(context).showSnackBar(SnackBar(content: Text("Skate spot added.")));
+    }
+    catch(error) {
+      Scaffold.of(context).showSnackBar(SnackBar(content: Text("Error: "+error.toString())));
+    }
+
   }
-  void addMarker() async {
+  void _addMarker() async {
+
+    String id = "";
+    String category = "";
+    GeoPoint coordinates;
+
+    editingMarker = true;
     var currentLocation = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
 
+    coordinates = new GeoPoint(currentLocation.latitude, currentLocation.longitude);
+
     setState(() {
       final marker = Marker(
+        draggable: true,
+        onDragEnd: (value) {
+          coordinates = new GeoPoint(value.latitude, value.longitude);
+        },
         markerId: MarkerId(locationCount.toString()),
         position: LatLng(currentLocation.latitude, currentLocation.longitude),
-        infoWindow: InfoWindow(title: 'Location '+locationCount.toString()),
+        onTap: () { setMarkerAttributesPopup(id,category,coordinates); },
       );
       markers.add(marker);
-      print("count: "+markers.length.toString());
-      locationCount++;
 
     });
+
+  }
+
+
+  void setMarkerAttributesPopup(String label, String category, GeoPoint coordinates) {
+
+    final textEditingController = new TextEditingController();
+    List<bool> isSelected = [false,false,false,false];
+    List<String> categories = ["Stairs","Gap","Rail","Ledge"];
+    @override
+    void dispose() {
+      textEditingController.dispose();
+      super.dispose();
+    }
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return new Dialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+          elevation: 16,
+          backgroundColor: Colors.grey,
+          child: Container(
+            height: 370.0,
+            width: 360.0,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+
+                  SizedBox(height: 20),
+
+                  Text('Set Attributes',
+                    style: GoogleFonts.rockSalt(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      shadows:
+                      [Shadow(color: Colors.black,offset: Offset(-2.5, -2.5))],
+                    ), // Font
+                  ),
+
+                  SizedBox(height: 20),
+
+                  ToggleButtons(
+                    borderColor: Colors.black,
+                    color: Colors.white,
+                    highlightColor: Colors.black,
+                    children: <Widget>[
+                      Text("Stairs"),
+                      Text("Gap"),
+                      Text("Rail"),
+                      Text("Ledge"),
+                    ],
+                    isSelected: isSelected,
+                    onPressed: (int index) {
+                      setState(() {
+                        for (int buttonIndex = 0; buttonIndex < isSelected.length; buttonIndex++) {
+                          if (buttonIndex == index) {
+                            isSelected[buttonIndex] = true;
+                            category = categories[buttonIndex];
+                          } else {
+                            isSelected[buttonIndex] = false;
+                          }
+                        }
+                      });
+                    },
+                  ),
+
+                  SizedBox(height: 20),
+
+                  Container(
+                    width: 300,
+                    child: TextField(
+                      controller: textEditingController,
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black,)
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blueGrey, width: 3.0),
+                          ),
+                          border: OutlineInputBorder(),
+                          focusColor: Colors.blueGrey,
+                          labelText: 'Label',
+                          labelStyle: TextStyle(color: Colors.white)
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  RaisedButton(
+                    color: Colors.blueGrey,
+                    elevation: 5,
+                    child: Text(
+                      "Add",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {
+                      if(!textIsEmpty(textEditingController.text)) {
+                        label = textEditingController.text;
+                        editingMarker = false;
+                        setState(() {
+                          markers.clear();
+                        });
+                        Navigator.of(context).pop();
+                        print("label: "+label+" category: "+category+" coords: ("+coordinates.latitude.toString()+","+coordinates.longitude.toString()+")");
+                        _saveSpot(category, label, coordinates);
+                      }
+                      else
+                        popupMessage("Label can\'t be empty.");
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  bool textIsEmpty(String text) {
+    if(text.replaceAll(" ", "").length > 0)
+      return false;
+    return true;
+  }
+
+  void popupMessage(String text) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return new Dialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+          elevation: 16,
+          backgroundColor: Colors.grey,
+          child: Container(
+            height: 200.0,
+            width: 300.0,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+
+                  SizedBox(height: 20),
+
+                  Text('Attention!',
+                    style: GoogleFonts.rockSalt(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      shadows:
+                      [Shadow(color: Colors.black,offset: Offset(-2.5, -2.5))],
+                    ), // Font
+                  ),
+
+                  SizedBox(height: 20),
+
+                  Text(text),
+
+                  SizedBox(height: 20),
+
+                  RaisedButton(
+                    color: Colors.blueGrey,
+                    elevation: 5,
+                    child: Text(
+                      "OK",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {
+                      //close popup
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 
   void setCameraToCurrentLocation(double lat,double lon) async {
@@ -175,10 +380,10 @@ class _SearchMap extends State<SearchMap> {
     );
   }
 
-  void _setStyle(GoogleMapController controller) async {
-    String value = await DefaultAssetBundle.of(context).loadString('assets/maps_style.json');
-    controller.setMapStyle(value);
-  }
+//  void _setStyle(GoogleMapController controller) async {
+//    String value = await DefaultAssetBundle.of(context).loadString('assets/maps_style.json');
+//    controller.setMapStyle(value);
+//  }
 
   void searchNearby(double latitude, double longitude) async {
     setState(() {
@@ -230,12 +435,12 @@ class _SearchMap extends State<SearchMap> {
     }
   }
 
-  showMenu() {
+  void showMenu() {
     showModalBottomSheet(
       elevation: 100,
         context: context,
         builder: (BuildContext context) {
-          return Container(
+          return new Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(16.0),
@@ -288,7 +493,10 @@ class _SearchMap extends State<SearchMap> {
             ),
           );
         });
+  }
 
-}
+  showPopup(String id, String category) {
+
+    }
 
 }
